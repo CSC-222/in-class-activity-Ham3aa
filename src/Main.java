fabd0d7670d4afb528353e1b909818745abc426b
Hamza Guzel <hamzaguzel@Hamzas-MacBook-Pro.local>
2023-03-29 21:24:26 -0400

    Updated Main.java file

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,14 @@
+import CSC_Racers.CSC_Racers;
+import CSC_Racers.Car;
+
 import java.io.FileNotFoundException;
 
 public class Main {
 
     public static void main(String[] args) throws FileNotFoundException {
 
+        CSC_Racers cscRacers = new CSC_Racers();
+        cscRacers.startGame();
         //Start Game Here
     }
 }
